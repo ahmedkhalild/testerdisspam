7e80c69e1cc720170d7253175a8a1ba97fa71e3b
ahmedkhalild <53646726+ahmedkhalild@users.noreply.github.com>
2019-08-03 10:56:41 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("607148874536058922")
+setInterval(function() {
+channel.send(`MeraDaqo is the Best ww MeraDaqo is the Best ww MeraDaqo is the Best ww`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
